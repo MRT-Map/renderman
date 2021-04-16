5a9a3e2dd33cbf4fc41700fb6c2b0ff0a545fb2d
iiiii7d <>
2021-04-16 13:09:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,104 @@
+import renderer
+from threading import Thread
+import flask
+import flask_cors
+import json
+import requests
+import cloudinary.uploader
+import cloudinary.api
+import os
+import io
+import gc
+import time
+import psutil
+from colorama import Fore, Style, init
+init()
+
+cloudinary.config( 
+    cloud_name = "mrt-map", 
+    api_key = "128387842344516", 
+    api_secret = os.environ['cloudinary']
+)
+
+def readFile(dir):
+    with open(dir, "r") as f:
+        data = json.load(f)
+        f.close()
+        return data
+
+def writeFile(dir, value):
+    with open(dir, "r+") as f:
+        f.seek(0)
+        f.truncate()
+        json.dump(value, f, indent=0)
+        f.close()
+
+def render():
+    p = json.loads(requests.get("https://api.npoint.io/5fcc99fc5028693a9569").text)
+    n = json.loads(requests.get("https://api.npoint.io/0db5b7881915de645ced").text)
+    s = readFile("skins/default.json")
+    #xMax, xMin, yMax, yMin = renderer.tools.plaJson_findEnds(p, n)
+    #tileList = renderer.tools.lineToTiles([(xMax,yMax),(xMin,yMax),(xMax,yMin),(xMin,yMin)], 5, 8, 32)
+    indexNew = {}
+    #for t in tileList:
+    gc.collect()
+    tiles = renderer.render(p, n, s, 5, 8, 32, saveImages=False)
+    gc.collect()
+    for tileName, tile in tiles.items():
+        tileBytes = io.BytesIO()
+        tile.save(tileBytes, format='PNG')
+        tileBytes.name = tileName.replace(", ", "_")+".png"
+        r = cloudinary.uploader.upload(tileBytes.getvalue(), public_id=tileName.replace(", ", "_"), overwrite=True, invalidate=True)
+        v = r['version']
+        print("Uploaded " + tileName)
+        indexNew[tileName.replace(", ", "_")] = v
+    index = readFile("index.json")
+    index.update(indexNew)
+    writeFile("index.json", index)
+
+app = flask.Flask('')
+
+@app.route('/', methods=['GET'])
+def main():
+    message = "je"
+    return flask.render_template('index.html', message=message)
+
+@app.route('/render/', methods=['POST'])
+def render_post():
+    render()
+    return "Complete"
+
+#@app.route('/tilev/', methods=['GET'])
+#@flask_cors.cross_origin(origin='replit.com')
+#def tilev():
+#    try:
+#        v = readFile("index.json")[flask.request.args.get("t")]
+#        response = flask.jsonify({'some': 'data'})
+#        response.headers.add('Access-Control-Allow-Origin', '*')
+#        return response
+#        return str(v)
+#    except KeyError:
+#        flask.abort(404)
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+    #flask_cors.CORS(app)
+
+def clean():
+    time.sleep(30)
+    print(Fore.YELLOW)
+    print("Before: " + str(gc.get_count()))
+    gc.collect()
+    print("After: " + str(gc.get_count()))
+    process = psutil.Process(os.getpid())
+    print(process.memory_info().rss/1000000)
+    print(Style.RESET_ALL)
+    time.sleep(30)
+
+server = Thread(target=run)
+server.start()
+
+cleaner = Thread(target=clean)
+cleaner.setDaemon(True)
+cleaner.start()
+#render()
